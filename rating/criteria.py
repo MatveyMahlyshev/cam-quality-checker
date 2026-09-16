a9e5d6fbc52420969f15d175993d0beb1fbf8320
m.mahlyshev <m.mahlyshev@citydrive.ru>
2026-09-17 00:23:46 +0300

    DEV-01 | Added criteries

---
+++ rating/criteria.py
@@ -0,0 +1,35 @@
+CRITERIA = {
+    "sharpness": True,
+    "noise": False,
+    "dynamic_range": True,
+    "exposure": True,
+    "contrast": True,
+    "colorfulness": True,
+    "chroma_noise": False,
+    "highlight_rec": True,
+    "shadow_detail": True,
+}
+
+DEFAULT_WEIGHTS = {
+    "sharpness": 0.20,
+    "noise": 0.15,
+    "dynamic_range": 0.15,
+    "exposure": 0.10,
+    "contrast": 0.10,
+    "colorfulness": 0.10,
+    "chroma_noise": 0.08,
+    "highlight_rec": 0.06,
+    "shadow_detail": 0.06,
+}
+
+REFERENCE = {
+    "sharpness": 8.0,
+    "noise": 2.0,
+    "dynamic_range": 6.5,
+    "exposure": 0.90,
+    "contrast": 0.20,
+    "colorfulness": 40.0,
+    "chroma_noise": 6.0,
+    "highlight_rec": 0.60,
+    "shadow_detail": 0.50,
+}
